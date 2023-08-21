512ce227a41f304c23bec6f50fae38886c510b76
ashok <ashok90olp8@gmail.com>
2023-08-22 01:58:25 +0530

    gird and background property added

---
+++ java.js
@@ -0,0 +1,8 @@
+ for (i=1;i<=256;i++){
+    
+content = document.createElement('div');
+content.classList.add('div')
+content.classList.add('color');
+container = document.querySelector('#container');
+container.appendChild(content);}
+
